ad4a8ac9f02c91b83194889bb22c5379d0c0995c
EmilGyllendahl <73532696+EmilGyllendahl@users.noreply.github.com>
2022-09-23 09:47:10 +0200

    Player Health file added
    Player health with a maximum and current player health.

---
+++ Worms-3D-implementation-assignment-main/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health; // Keeps track of the players current health
+    public int maxHealth = 10; // How much health you have when you are at full health
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    public void TakeDamage(int amount) // This function will be called anytime the player takes damage/ amount = how much damage the player takes.
+    {
+        health -= amount;
+        if(health <= 0)
+        {                           // If the damage takes the player down to zero or below the the player will be destoyed
+            Destroy(gameObject);
+        }
+    }
+}
