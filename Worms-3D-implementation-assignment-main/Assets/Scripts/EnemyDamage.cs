1a91672e7d4014db30e6117a9c220a7cec222061
EmilGyllendahl <73532696+EmilGyllendahl@users.noreply.github.com>
2022-09-23 09:46:07 +0200

    Enemy Damage file
    Code for making the enemy taking damage and losing health, when the health is set to 0 the playerobject is destroyed.

---
+++ Worms-3D-implementation-assignment-main/Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    public PlayerHealth playerHealth;  // This lets the EnemyDamage script know where to find the PlayerHealth script in Unity
+    public int damage = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("Bullet");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision) // This function is called whenever something enters the enemy´s collider.
+    {
+        if(collision.gameObject.name == "Bullet")
+        {
+            playerHealth.TakeDamage(damage); // Talks to the PlayerHealth scripts/ and then to the TakeDamage function, health will subtract by 1 each hit.
+        }
+    }
+}
